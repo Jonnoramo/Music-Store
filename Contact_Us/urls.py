43a3ee7a47b0286d117ac742d8d379db3e0dfebe
Jonathan Ramsden <ramsden_118@hotmail.com>
2018-04-19 20:23:20 +0100

    first commit

---
+++ Contact_Us/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url('email/', views.emailView, name='email'),
+    url('success/', views.successView, name='success'),
+]
