8fb378e3e6571d3c656f2736f251fc21217dac7e
Jonnoramo <Ramsden_118@hotmail.com>
2018-04-23 13:48:14 +0100

    Changed Contact_Us/email.html to email.html

--- Contact_Us/views.py
+++ Contact_Us/views.py
@@ -17,7 +17,7 @@ def emailView(request):
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
-    return render(request, "Contact_Us/email.html", {'form': form})
+    return render(request, "email.html", {'form': form})
 
 def successView(request):
     return HttpResponse('Success! Thank you for your message.')
